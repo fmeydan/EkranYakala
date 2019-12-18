335548f00bbe8b1d95e00126ac2c3c701723a2dd
Fuat Meydan <meydan.fuat@outlook.com>
2019-12-18 05:18:47 +0300

    Basit Ekran Görüntüsü alma uygulaması

--- EkranYakala/Form1.Designer.cs
+++ EkranYakala/Form1.Designer.cs
@@ -28,13 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(20, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Yakala";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(190, 95);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Location = new System.Drawing.Point(101, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Farklı Yere";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(10, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(196, 131);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(218, 152);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(200, 190);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ekran Yakala";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- EkranYakala/Form1.cs
+++ EkranYakala/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,74 @@ namespace EkranYakala
         public Form1()
         {
             InitializeComponent();
+        }
+       
+        string yol;  //Farklı fonksiyonlardan müdahale edebilmek için globalde yol u tutan string değişken.
+        
+        
+
+
+        public void button1_Click(object sender, EventArgs e) //Yakala butonunun tıklanma fonksiyonu.
+        {
+            this.Hide(); //Yakalanan ekranda bulunmasın diye gizliyoruz.
+            System.Threading.Thread.Sleep(500); //Pencerenin gizlenmesi için bir süre işlemi bekletiyoruz.
+            Bitmap bm = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height); //Yakalanan görüntünün en boy oranını sistemimizin en boy oranıyla aynı olmasını sağlıyoruz. İki monitörlü sistemlerde Birincil monitörü yakalar.
+            Graphics g = Graphics.FromImage(bm); //Bitmap ile alınan ekran verilerini görselleştiriyoruz.
+            g.CopyFromScreen(0,0,0,0,bm.Size); //Görseli aldığımız ekran verilene göre oluşturuyoruz.
+            pictureBox1.Image = bm; //oluşturduğumuz image box a küçük bir önizlemesini yerleştiriyoruz.
+            string dosya_adi = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"); //dosya adının benzersiz olması için sistem tarih ve saatini kullanıyoruz.
+            yol = Properties.Settings.Default.yol; //program ilk açılırken sakladığımız klasör bilgisini yol değişkenine atıyoruz.
+
+            //MessageBox.Show(yol + dosya_adi); Kontrol için
+            bm.Save(yol + "\\" + dosya_adi + ".jpg"); //oluiturduğumuz görüntüyü daha önceden belirlediğimiz yola kaydediyoruz.
+            this.Show(); //penceremizi tekrar görünür hale getiriyoruz.
+        }
+
+        public void button2_Click(object sender, EventArgs e) //Yer Değiştir butonunun tıklanma fonksiyonu.
+        {
+            DialogResult result = MessageBox.Show("Kaydetme Yerini Seçin", "Ekran Yakala", MessageBoxButtons.OK); 
+            if (result == DialogResult.OK)
+            {
+                SaveFileDialog sf = new SaveFileDialog(); //Kaydetme yerinin seçilebilmesi için açılan dialog penceresi.
+                sf.InitialDirectory = "C:\\"; //Açılan dialog penceresinin ilk başlayacağı yeri C: olarak belirledik.
+                sf.FileName = "Hedef Klasörü Seçin"; // Normalde burası seçilecek dosyanın ismini ön tanımlı olarak yazmamız gereken yer fakat burda dosya seçmekten ziyade klasör seçtiğimiz için buranın dokunulmasına gerek yok. Boş kalmaması ve kullanıcıyı biraz olsun yönlendirmek için böyle yazdım.
+
+                if (sf.ShowDialog() == DialogResult.OK) // Klasör seçme penceresinin save tuşuna basılınca çalışacak kodlar.
+                {
+                    yol = Path.GetDirectoryName(sf.FileName); //Seçilen klasörün yolu yol değişkenine atanır.
+                    Properties.Settings.Default.yol = yol; //yol'un hafızada kalması için uygulama Propertiesine kaydediyoruz.
+                    Properties.Settings.Default.Save(); //Properties e kaydetme işlmeni onaylıyoruz.
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {           
+            
+            string son_kayitli_yol=Properties.Settings.Default.yol;// uygulama properties'indeki yol u "son_kayitli_yol" değişkenine atıyoruz.            
+            if (son_kayitli_yol==null) //bu değişken boş veya dolumu kontrolü yapıyoruz. eğer boş ise henüz yol belirtilmemiş demektir. 
+            {
+                DialogResult result = MessageBox.Show("Kaydetme Yerini Seçin", "Ekran Yakala", MessageBoxButtons.OK);
+                if (result == DialogResult.OK)
+                {
+                    SaveFileDialog sf = new SaveFileDialog(); //Kaydetme yerinin seçilebilmesi için açılan dialog penceresi.
+                    sf.InitialDirectory = "C:\\"; //Açılan dialog penceresinin ilk başlayacağı yeri C: olarak belirledik.
+                    sf.FileName = "Hedef Klasörü Seçin";// Normalde burası seçilecek dosyanın ismini ön tanımlı olarak yazmamız gereken yer fakat burda dosya seçmekten ziyade klasör seçtiğimiz için buranın dokunulmasına gerek yok. Boş kalmaması ve kullanıcıyı biraz olsun yönlendirmek için böyle yazdım.
+                    if (sf.ShowDialog() == DialogResult.OK)// Klasör seçme penceresinin save tuşuna basılınca çalışacak kodlar.
+                    {
+                        yol = Path.GetDirectoryName(sf.FileName); //Seçilen klasörün yolu yol değişkenine atanır.
+                        Properties.Settings.Default.yol = yol;//yol'un hafızada kalması için uygulama Propertiesine kaydediyoruz.
+                        Properties.Settings.Default.Save(); //Properties e kaydetme işlmeni onaylıyoruz.
+                    }
+                }
+            }
+            else
+            {
+                this.Show(); //uygulama penceremiz görüntülenir.
+            }
+            
+          
+            
         }
     }
 }
